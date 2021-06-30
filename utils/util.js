403a0c0a4ee2232a4757cde42b26993d936f7fde
SEIFU MEKONNEN TOLOSA <tms.18@yahoo.com>
2021-06-30 11:38:21 +0100

    add evm time methods

--- utils/util.js
+++ utils/util.js
@@ -1,6 +1,18 @@
 
 function toBytes(input)  { return ethers.utils.formatBytes32String(input);}
 
+async function evm_increaseTime(time) {
+    await network.provider.send("evm_increaseTime", [time]);
+    await network.provider.send("evm_mine");
+}
+
+async function evm_setNextBlockTimestamp(blockTimestamp) {
+    await network.provider.send("evm_setNextBlockTimestamp", [blockTimestamp]);
+    await network.provider.send("evm_mine");
+}
+
 module.exports = {
-    toBytes
+    toBytes,
+    evm_increaseTime,
+    evm_setNextBlockTimestamp
 }
