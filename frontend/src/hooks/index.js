394071a9d0868b466e07e685ed03391e366ddd13
SEIFU MEKONNEN TOLOSA <tms.18@yahoo.com>
2021-07-04 11:13:18 +0100

    Add hook and helper

--- frontend/src/hooks/index.js
+++ frontend/src/hooks/index.js
@@ -1,1 +1,3 @@
 export { default as useContractLoader } from "./ContractLoader";
+export { default as useUserSigner } from "./UserSigner";
+export { default as useBurnerSigner } from "./BurnerSigner";
